85651ad0b29463aeff95314689e210d2399c26e7
gassdev <gassdev@gmail.com>
2022-08-23 01:08:09 +0000

    feat: make use of react-flip-toolkit package for the content area

--- assets/modules/Filter.js
+++ assets/modules/Filter.js
@@ -1,3 +1,5 @@
+import { Flipper, spring } from 'flip-toolkit'
+
 export default class Filter {
   /**
    * @property {HTMLElement} pagination
@@ -5,7 +7,6 @@ export default class Filter {
    * @property {HTMLElement} sorting
    * @property {HTMLFormElement} form
    */
-
   /**
    * @param {HTMLElement | null} element
    */
@@ -50,20 +51,100 @@ export default class Filter {
   }
 
   async loadUrl(url) {
-    const ajaxUrl = url + '&ajax=1'
-    const response = await fetch(ajaxUrl, {
+    this.showLoader()
+    const params = new URLSearchParams(url.split('?')[1] || '')
+    params.set('ajax', 1)
+    const response = await fetch(url.split('?')[0] + '?' + params.toString(), {
       headers: {
         'X-Requested-With': 'XMLHttpRequest',
       },
     })
     if (response.status >= 200 && response.status < 300) {
       const data = await response.json()
-      this.content.innerHTML = data.content
+      this.flipContent(data.content)
       this.sorting.innerHTML = data.sorting
       this.pagination.innerHTML = data.pagination
-      history.replaceState({}, '', url)
+      params.delete('ajax')
+      history.replaceState({}, '', url.split('?')[0] + '?' + params.toString())
     } else {
       console.error(response)
     }
+    this.hideLoader()
+  }
+
+  /**
+   * Remplace les éléments de la grille avec un effet d'animation flip
+   * @param {string} content
+   */
+  flipContent(content) {
+    const springConfig = 'gentle'
+    const exitSpring = function (element, index, onComplete) {
+      spring({
+        config: 'stiff',
+        values: {
+          translateY: [0, -20],
+          opacity: [1, 0],
+        },
+        onUpdate: ({ translateY, opacity }) => {
+          element.style.opacity = opacity
+          element.style.transform = `translateY(${translateY}px)`
+        },
+        onComplete,
+      })
+    }
+    const appearSpring = function (element, index) {
+      spring({
+        config: 'stiff',
+        values: {
+          translateY: [20, 0],
+          opacity: [0, 1],
+        },
+        onUpdate: ({ translateY, opacity }) => {
+          element.style.opacity = opacity
+          element.style.transform = `translateY(${translateY}px)`
+        },
+        delay: index * 20,
+      })
+    }
+    const flipper = new Flipper({ element: this.content })
+    Array.from(this.content.children).forEach((element) => {
+      flipper.addFlipped({
+        element,
+        spring: springConfig,
+        flipId: element.id,
+        shouldFlip: false,
+        onExit: exitSpring,
+      })
+    })
+    flipper.recordBeforeUpdate()
+    this.content.innerHTML = content
+    Array.from(this.content.children).forEach((element) => {
+      flipper.addFlipped({
+        element,
+        spring: springConfig,
+        flipId: element.id,
+        onAppear: appearSpring,
+      })
+    })
+    flipper.update()
+  }
+
+  showLoader() {
+    this.form.classList.add('is-loading')
+    const loader = this.form.querySelector('.js-loading')
+    if (loader === null) {
+      return
+    }
+    loader.setAttribute('aria-hidden', 'false')
+    loader.style.display = null
+  }
+  hideLoader() {
+    this.form.classList.remove('is-loading')
+    const loader = this.form.querySelector('.js-loading')
+    if (loader === null) {
+      return
+    }
+    loader.setAttribute('aria-hidden', 'true')
+    loader.style.display = 'none'
   }
 }
